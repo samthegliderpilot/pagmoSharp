ee8de245c4c40ca1c706831410c3597b1c87e0a8
sam <samthegliderpilot@gmail.com>
2022-08-16 21:03:46 -0400

    Cleanup and getting it to work on a new computer - Upgrading to .net 6 - Removing extra .i file - Moving where Swig.exe lives

--- createSwigWrappersAndPlaceThem.bat
+++ createSwigWrappersAndPlaceThem.bat
@@ -1,7 +1,7 @@
 REM runs swig and copies files around automatically
 REM note that I hard code a path to swig.exe.  This will obviously fail if it isn't there...
 
-C:\src\swigwin-4.0.2\swig.exe -c++ -csharp -namespace pagmo -dllimport pagmoWrapper.dll swigInterfaceFileAndPagmoHeaders\pagmoSharpSwigInterface.i
+C:\Programs\swigwin-4.0.2\swig.exe -c++ -csharp -namespace pagmo -dllimport pagmoWrapper.dll swigInterfaceFileAndPagmoHeaders\pagmoSharpSwigInterface.i
 copy swigInterfaceFileAndPagmoHeaders\pagmoSharpSwigInterface_wrap.cxx "pagmoWrapper\GeneratedWrappers.cxx"
 copy swigInterfaceFileAndPagmoHeaders\pagmoSharpSwigInterface_wrap.h "pagmoWrapper\pagmoSharpSwigInterface_wrap.h"
 
